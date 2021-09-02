e52561ccac2533394acfee8e72b76cd7ec235fd6
Morshed-Ahmed <morshed50r@gmail.com>
2021-09-02 11:17:07 +0600

    code update

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const searchBook = () => {
     const searchInput = document.getElementById('srarch-input');
     const searchText = searchInput.value;
+    searchInput.value = " ";
+
     //console.log(searchText)
     const url = ` http://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
@@ -11,6 +13,10 @@ const searchBook = () => {
 const bookDisply = books => {
     // console.log(books)
     const searchResult = document.getElementById('search-result')
+    searchResult.textContent = " ";
+    if (books.length == 0) {
+        console.log('sorry')
+    }
     for (const book of books) {
         console.log(book)
         const div = document.createElement('div')
@@ -19,9 +25,10 @@ const bookDisply = books => {
         
         
                 <div class="card-body">
+                <img  src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class=" card-img-top" alt="...">
                     <h5 class="card-title">Book Name : ${book.title}</h5>
                     <h5 class="card-title">Author Name : ${book.author_name}</h5>
-                    <h5 class="card-title">Publish Date : ${book.publish_date}</h5>
+                    <h5 class="card-title">Publish Date : ${book.first_publish_year}</h5>
                     
                     
 
